4e01ec461cbd4db16a57b6e97df6a0c3dab1ae4b
plolek123 <102098812+plolek123@users.noreply.github.com>
2025-06-17 15:10:36 +0200

    działające sketch_jun17a.ino

--- sketch_jun17a.ino
+++ sketch_jun17a.ino
@@ -1,52 +1,50 @@
 #define RELAY_COUNT 8
 const int relayPins[RELAY_COUNT] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-#define BUTTON_OFF A0
-#define BUTTON_ON  A1
-
-bool lastStateOff = HIGH;
-bool lastStateOn  = HIGH;
-
-// Jeśli przekaźniki są aktywne stanem NISKIM (LOW), ustaw na true:
+#define BUTTON_PIN A0
 const bool RELAY_ACTIVE_LOW = true;
+
+bool isRunning = false;
+bool lastButtonState = HIGH;
+unsigned long lastChangeTime = 0;
+const unsigned long interval = 300; // czas między losowaniami
 
 void setup() {
   randomSeed(analogRead(0));
-
   for (int i = 0; i < RELAY_COUNT; i++) {
     pinMode(relayPins[i], OUTPUT);
     digitalWrite(relayPins[i], relayOff());
   }
-
-  pinMode(BUTTON_OFF, INPUT_PULLUP);
-  pinMode(BUTTON_ON, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  bool currentStateOff = digitalRead(BUTTON_OFF);
-  bool currentStateOn  = digitalRead(BUTTON_ON);
+  // Obsługa przycisku (kliknięcie)
+  bool currentState = digitalRead(BUTTON_PIN);
+  if (lastButtonState == HIGH && currentState == LOW) {
+    isRunning = !isRunning;
 
-  // Wyłączenie LED-ów
-  if (lastStateOff == HIGH && currentStateOff == LOW) {
+    if (!isRunning) {
+      // Jeśli wyłączamy: gasimy wszystko
+      for (int i = 0; i < RELAY_COUNT; i++) {
+        digitalWrite(relayPins[i], relayOff());
+      }
+    }
+  }
+  lastButtonState = currentState;
+
+  // W trybie włączonym: losujemy co 1 sekundę
+  if (isRunning && millis() - lastChangeTime >= interval) {
+    lastChangeTime = millis();
     for (int i = 0; i < RELAY_COUNT; i++) {
-      digitalWrite(relayPins[i], relayOff());
+      digitalWrite(relayPins[i], random(0, 2) ? relayOn() : relayOff());
     }
   }
 
-  // Wylosowanie nowego koloru
-  if (lastStateOn == HIGH && currentStateOn == LOW) {
-    for (int i = 0; i < RELAY_COUNT; i++) {
-      int r = random(0, 2); // 0 lub 1
-      digitalWrite(relayPins[i], r ? relayOn() : relayOff());
-    }
-  }
-
-  lastStateOff = currentStateOff;
-  lastStateOn  = currentStateOn;
-
-  delay(50); // mały debounce
+  delay(20); // lekki debounce
 }
 
+// Pomocnicze funkcje logiczne
 int relayOn() {
   return RELAY_ACTIVE_LOW ? LOW : HIGH;
 }
